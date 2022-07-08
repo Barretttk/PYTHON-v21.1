feb298212073e0ecc75456de12d14b5ca4c81d3d
Timothy Barrett <Timothy.barrett@hotmail.com>
2022-07-07 20:33:28 -0400

    commit

--- fundamentals/fundamentals/under/understanding_r.py
+++ fundamentals/fundamentals/under/understanding_r.py
@@ -4,3 +4,22 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
+@app.route('/dojo')
+def dojo():
+    return "dojo!"
+
+@app.route('/hi/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hi(name):
+    print(name)
+    return f"<h1>Hi, {name}</h1>"
+
+@app.route('/repeat/<roll>/<name>')
+def repeat(roll,name):
+    # name= "yeah<br>"
+    print(name)
+    return (name * int(roll))
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)
+
