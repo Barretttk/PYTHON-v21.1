d0d37abf92baa295beb803a4800d6ba2b52f3179
Timothy Barrett <Timothy.barrett@hotmail.com>
2022-07-10 22:52:14 -0400

    commit

---
+++ assignments/Bank/BankAccount.py
@@ -0,0 +1,12 @@
+class BankAccount:
+
+    def __init__(self) -> None:
+        pass
+
+    def deposite(self, amount):
+
+    def withdraw(self, amount):
+
+    def display_account_info(self):
+
+    def yield_interest(self):
