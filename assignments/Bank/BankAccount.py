49281b6636eb02f7f81a2fd26107f6e9cba3577a
Timothy Barrett <Timothy.barrett@hotmail.com>
2022-07-11 13:59:01 -0400

    commit

--- assignments/Bank/BankAccount.py
+++ assignments/Bank/BankAccount.py
@@ -1,12 +1,33 @@
-class BankAccount:
+from typing_extensions import Self
 
-    def __init__(self) -> None:
+
+
+
+class BankAccount: 
+
+
+    def __init__(self,int_rate, balance):
         pass
+    Self.ammount
+
 
     def deposite(self, amount):
+        sum1 =(amount + self)
+        return sum1
+    print("new balance");
+
+
 
     def withdraw(self, amount):
+        withdraw = sub (amount -- self)
+    if sum2 is < sum1:
+    print("Insufficient funds: Charging a $5 fee and deduct $5")
+
+
 
     def display_account_info(self):
+        sum1 = balance
+    print(f"balance"+ sum1)
 
     def yield_interest(self):
+    print(balance)
