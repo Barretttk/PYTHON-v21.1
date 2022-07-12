96634170122c2c4634f453326ec0fbe39de3e22f
Timothy Barrett <Timothy.barrett@hotmail.com>
2022-07-11 20:51:01 -0400

    commit

---
+++ Practice/Practice/user.py
@@ -0,0 +1,29 @@
+
+class User:
+    def __init__(self) -> None:
+        self.first_name = "Tim"
+        self.last_name = "Barrett"
+        self.email = "Tim@gmail.com"
+        self.age = 38
+        self.is_rewards_member - False
+        # default valuse of false
+        self.gold_card_point = 0
+
+def display_info(self):
+    
+    print(self)
+
+def enroll(self):
+
+    print()
+
+def Spend_point(self,amount):
+
+    print()
+
+
+
+tim = User ()
+tim.display_info()
+# user_cam = User_2()
+# print(self.first_name)
